9f36b45c3778bb840d15f7418b104869aa5190bd
Crazy24699 <110395759+Crazy24699@users.noreply.github.com>
2024-06-04 12:35:01 +0200

    Major Changes

---
+++ Painted Boomerang/Assets/Scripts/Enemy AI/Nodes/BTDeadNode.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTDeadNode : BTNodeBase
+{
+    protected EnemyAI EnemyAIScript;
+    public BTDeadNode(GameObject EnemyAIRef)
+    {
+        EnemyAIScript = EnemyAIRef.GetComponent<EnemyAI>();
+    }
+
+    public override NodeStateOptions RunNodeLogicAndStatus()
+    {
+        //if (EnemyAIScript.IsDead)
+        //{
+        //    Debug.Log("bulletproog");
+        //    return NodeStateOptions.Running;
+        //}
+        return NodeStateOptions.Fail;
+    }
+}
