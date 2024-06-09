4747f9cd6ffbaf739f7e7cf58a093d3a028da7c0
Mattdevill <110395675+Mattdevill@users.noreply.github.com>
2024-06-09 13:57:18 +0200

    added SFX
    they dont work and i dont want to break anything

--- Painted Boomerang/Assets/Scripts/AudioManager.cs
+++ Painted Boomerang/Assets/Scripts/AudioManager.cs
@@ -6,16 +6,19 @@ using UnityEngine.UIElements;
 public class AudioManager : MonoBehaviour
 {
     //Audio sources
+    [Header("----------Sources----------")]
     [SerializeField] AudioSource musicSource;
     [SerializeField] AudioSource SFXSource;
 
     //Audio clips
+    [Header("----------Clips----------")]
     //Music
     public AudioClip musicClip;
 
     //SFX
-
-
+    public AudioClip spinClip;
+    public AudioClip moveClip;
+    public AudioClip hitClip;
 
     private void Awake()
     {
